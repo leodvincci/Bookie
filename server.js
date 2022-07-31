0cc7ec5dc949b750bbf915e6e30b3b3690855e9c
leodvincci <lpenrose@depaul.edu>
2022-07-31 06:19:50 -0500

    updated dependency

--- server.js
+++ server.js
@@ -1,14 +1,51 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 let PORT = 3042;
-express.static("public")
-app.use(express.static('public'))
+let myDatabase = "bookieDatabase";
+let myCollection = "bookieBooks";
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'));
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://demodatabase:demodatabase123321@bookie.9gqfpoa.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 
+
+
+
+
+
+///////// routes/////////////
 
 app.get("/",(request,respond)=>{
     respond.send(`Listening on Port ${PORT}`);
 })
+
+
+app.post("/postbook",(request,respond)=>{
+
+    respond.send("You Posted A new book!! Congrats!")
+
+    // client.db(myDatabase).collection(myCollection).insertOne(
+    //     { bookTitle:"Inspired",bookAuthor:"Marty Cagan",bookImg: "https://covers.openlibrary.org/b/id/9700654-L.jpg", bookLikes:0,bookDislikes:0 }
+    // )
+
+
+})
+
+
+
+
+
+
+////////////end of routes//////////
 
 
 app.listen(PORT, ()=>{

88106f5e9f8eeefa30712c6fba506bb04aa716fe
leodvincci <lpenrose@depaul.edu>
2022-08-01 15:34:41 -0500

    added aws s3 bucket

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+
+const { getSignedUrl }  = require( "@aws-sdk/s3-request-presigner");
+const { S3Client, GetObjectCommand } = require( "@aws-sdk/client-s3");
+
+
+
+const dotenv = require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
@@ -21,7 +28,27 @@ client.connect(err => {
     const collection = client.db("test").collection("devices");
     // perform actions on the collection object
 });
+const aws = require("@aws-sdk/client-s3")
+const {PutObjectCommand} = require("@aws-sdk/client-s3");
+const Crypto = require("crypto");
+const crypto = require("crypto");
+const bucketName = process.env.BUCKET_NAME;
+const bucketRegion = process.env.BUCKET_REGION;
+const accessKey= process.env.ACCESS_KEY;
+const secretAccessKey= process.env.SECRET_ACCESS_KEY;
 
+
+console.log(process.env.BUCKET_REGION)
+
+const s3 = new S3Client(
+    {
+        credentials:{
+            accessKeyId:accessKey,
+            secretAccessKey: secretAccessKey,
+        },
+        region:bucketRegion
+    }
+);
 
 
 
@@ -30,22 +57,52 @@ client.connect(err => {
 
 ///////// routes/////////////
 
-app.get("/",(request,respond)=>{
+
+let theData;
+
+app.get("/",(request,respond)=> {
     console.log("Getting All Books...")
+
     client.db(myDatabase).collection(myCollection).find().toArray()
-        .then(r =>{
-            respond.render("index.ejs", {
-                "books":r
-            });
-            console.log(r)
+        .then( async data => {
+            for (const post of data) {
+                const getObjectParams = {
+                    Bucket: bucketName,
+                    Key: post.bookImg,
+                }
+                const command = new GetObjectCommand(getObjectParams);
+                const url = await getSignedUrl(s3, command, {expiresIn: 300});
+                post.imageUrl = url
+                console.log("URL: " + url)
+            }
+            // respond.send(data)
+
+            respond.render("index.ejs",{
+                books:data
+            })
         })
+
+
+
 })
 
 
 app.post("/postbook", upload.single("bookCover") ,(request,respond)=>{
+    const randomImageName = crypto.randomBytes(32).toString('hex')
+    console.log("Random Name: " + randomImageName)
+    const params = {
+        Bucket:bucketName,
+        Key: randomImageName,
+        Body: request.file.buffer,
+        ContentType: request.file.mimetype,
+    }
+
+    const command = new PutObjectCommand(params)
+
+    s3.send(command).then(r =>(console.log("book cover uploaded")) )
 
     client.db(myDatabase).collection(myCollection).insertOne(
-        {bookTitle:request.body.bookTitle,bookAuthor:request.body.bookAuthor, bookImg: request.file.buffer, bookLikes:0, bookDislikes:0 }
+        {bookTitle:request.body.bookTitle,bookAuthor:request.body.bookAuthor, bookImg:randomImageName, bookLikes:0, bookDislikes:0 }
     )
         .then(()=>{
             console.log("New Book Added")
@@ -60,18 +117,18 @@ app.post("/postbook", upload.single("bookCover") ,(request,respond)=>{
 
 
 app.put("/updatelike",(request,response)=>{
-    console.log("Passed ID: " + request.body.theBookId)
-    client.db(myDatabase).collection(myCollection).findOneAndUpdate(
-        { _id: ObjectId(request.body.theBookId)},
-        {
-            $set: {
-                bookLikes: request.body.theBookLikeCount,
-                bookDislikes:request.body.theBookDislikeCount
+        console.log("Passed ID: " + request.body.theBookId)
+        client.db(myDatabase).collection(myCollection).findOneAndUpdate(
+            { _id: ObjectId(request.body.theBookId)},
+            {
+                $set: {
+                    bookLikes: request.body.theBookLikeCount,
+                    bookDislikes:request.body.theBookDislikeCount
+                }
+            },
+            {
+                upsert: true
             }
-        },
-        {
-            upsert: true
-        }
 
 
         )
